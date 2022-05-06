215624db8df0decf0f75105c695d862f12ace209
Diogo Freitas <dntfreitas123@gmail.com>
2022-05-06 09:35:49 +0100

    Code refactoring and date parsing. GH Action also trigers on PR and push to main

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 import requests as req
@@ -6,36 +7,38 @@ from bs4 import BeautifulSoup
 from keys import *
 
 # Requesting for the website
-Web = req.get('https://www.madeira.gov.pt/drett')
+web = req.get('https://www.madeira.gov.pt/drett')
 
 # Creating a BeautifulSoup object and specifying the parser
-S = BeautifulSoup(Web.text, 'html.parser')
+s = BeautifulSoup(web.text, 'html.parser')
 
 # Find the id with gas info
-spanWithGasInfo = S.find('span', {'id': 'dnn_ctr9883_View_D1_dlstInformacaoOne_Conteudo_0'})
+span_with_gas_info = s.find('span', {'id': 'dnn_ctr9883_View_D1_dlstInformacaoOne_Conteudo_0'})
 
 # Get the text without html tags
-gasInfo = spanWithGasInfo.get_text('\n', strip=True)
-arr_info = gasInfo.split('\n')
+gas_info = span_with_gas_info.get_text('\n', strip=True).split('\n')
 
 # Parse the data
 dict_prices = {}
 last_id = 0
 i = 0
-while i < len(arr_info) - 1:
+while i < len(gas_info) - 1:
     # Get date
-    add_date = i == 0 or arr_info[i].startswith(NEW_DATE_KEY)
-    if arr_info[i].startswith(NEW_DATE_KEY):
+    add_date = i == 0 or gas_info[i].startswith(NEW_DATE_KEY)
+    if gas_info[i].startswith(NEW_DATE_KEY):
         i += 1
         last_id += 1
     if add_date:
-        start_date, end_date = arr_info[i].replace('.', '-').split('\xa0a ')
+        start_date, end_date = gas_info[i].replace('.', '-').split('\xa0a ')
+        # Parse date
+        start_date = datetime.datetime.strptime(start_date, '%d-%m-%Y').strftime('%Y-%m-%d')
+        end_date = datetime.datetime.strptime(end_date, '%d-%m-%Y').strftime('%Y-%m-%d')
         dict_prices[last_id] = {START_DATE_KEY: start_date, END_DATE_KEY: end_date, GAS_KEY: []}
         i += 1
     # Gas information
     else:
-        gas = arr_info[i]
-        price = float(arr_info[i + 1][:-1].replace(',', '.'))
+        gas = gas_info[i]
+        price = float(gas_info[i + 1][:-1].replace(',', '.'))
         dict_prices[last_id][GAS_KEY].append({GAS_NAME_KEY: gas, GAS_PRICE_KEY: price})
         i += 2
 
