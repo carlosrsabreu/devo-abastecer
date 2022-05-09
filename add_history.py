c8a77b175ccd07f5aefc141819b43ab53ead7737
Diogo Freitas <dntfreitas123@gmail.com>
2022-05-09 20:42:53 +0100

    Fix append to history file

--- add_history.py
+++ add_history.py
@@ -8,10 +8,13 @@ with open('gas_info.json', encoding='utf-8') as f:
     next_week = json.load(f)[NEXT_WEEK]
     key_next_week = next_week[START_DATE_KEY]
 
-# Get history
-with open('gas_info_history.json', encoding='utf-8') as f:
+# Get history and clear the file
+with open('gas_info_history.json', 'r+', encoding='utf-8') as f:
     history_dict = json.load(f)
     history_dict[key_next_week] = next_week
+    pass
 
-# Print the information
-print(json.dumps(history_dict, indent=1, ensure_ascii=False))
+# Set the history
+with open('gas_info_history.json', 'w', encoding='utf-8') as f:
+    json_str = json.dumps(history_dict, indent=1, ensure_ascii=False)
+    f.write(json_str)
