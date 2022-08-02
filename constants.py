e7068ae2959ce4da516b5fc4351249e7676b4e14
Diogo Freitas <dntfreitas123@gmail.com>
2022-08-02 01:09:53 +0100

    Fix for issue #6

---
+++ constants.py
@@ -0,0 +1,28 @@
+# Values
+END_DATE_KEY = 'End date'
+GAS_KEY = 'Gas'
+GAS_NAME_KEY = 'Name'
+GAS_PRICE_KEY = 'Price'
+NEW_DATE_KEY = '.......'
+START_DATE_KEY = 'Start date'
+CURRENT_WEEK = 'current'
+PREVIOUS_WEEK = 'previous'
+# Constants
+DIFFERENCE_95_98_PRICE = 0.15
+# Endpoint
+ENDPOINT = 'https://www.madeira.gov.pt/drett'
+# Span
+SPAN_ID = 'dnn_ctr9883_View_D1_dlstInformacaoOne_Conteudo_0'
+# Files
+CURRENT_GAS_INFO_FILE = 'gas_info.json'
+CURRENT_GAS_HISTORY_JSON_FILE = 'history/gas_info_history.json'
+CURRENT_GAS_HISTORY_CSV_FILE = 'history/gas_info_history.csv'
+# Gas
+DIESEL = 'Gasóleo Rodoviário'
+GASOLINE_95 = 'Gasolina IO95'
+GASOLINE_98 = 'Gasolina IO98'
+COLORED_DIESEL = 'Gasóleo Colorido e Marcado'
+# Gas (for Tweet)
+DIESEL_TW = 'Gasóleo         '
+GASOLINE_95_TW = 'Gasolina 95  '
+GASOLINE_98_TW = 'Gasolina 98  '
