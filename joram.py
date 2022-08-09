9397fb3dffadfa52fce358bdd00d391f2fb9fc45
Leo Oliveira <leo@13dev.pt>
2022-08-09 11:43:09 +0100

    Joram: Make a simple struct data to associate name to prices, yield every result price (#5)

--- joram.py
+++ joram.py
@@ -1,17 +1,18 @@
+#!/bin/python
 import re
 from io import BytesIO
 import requests
 from PyPDF2 import PdfFileReader
+from constants import GASOLINE_95, DIESEL, COLORED_DIESEL
 
 # TODO: Get url with current year
 #       Get all pdf's with the current date
 #       Search for gas prices
 #       Script will run only on friday every 1h to check for new documents
 
-
 PDF_GAS_PRICE_REGEX = r'(?<=€ )([\d,]+)(?= por litro)'
 
-# TODO: Only for example. Make this url dynamic with current year and year
+# TODO: Only for example. Make this url dynamic with current year and date
 JORAM_URL = 'https://joram.madeira.gov.pt/joram/2serie/Ano%20de%202022/IISerie-138-2022-07-22Supl.pdf'
 
 
@@ -29,13 +30,22 @@ def read_pdf_prices(url):
     The prices are always in the same order just need to make a match.
     """
     response = requests.get(url)
-    prices = ()
     for line in get_pdf_content_lines(response.content):
         matches = re.search(PDF_GAS_PRICE_REGEX, line)
         if matches:
-            prices += (matches.group(),)
-
-    print(prices)
+            yield matches.group()
 
 
-read_pdf_prices(JORAM_URL)
+def get_gas_prices(url):
+    """
+    Attach the prices to a label
+    :param url: Pdf url that contains gas prices
+    :return: {'Gasolina IO95': '1,889', 'Gasóleo Rodoviário': '1,789', 'Gasóleo Colorido e Marcado': '1,425'}
+    """
+    labels = (GASOLINE_95, DIESEL, COLORED_DIESEL)
+
+    return dict(zip(labels, read_pdf_prices(url)))
+
+
+if __name__ == '__main__':
+    print(get_gas_prices(JORAM_URL))
