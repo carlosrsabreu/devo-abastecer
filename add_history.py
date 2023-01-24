43633d2a4449eed80c4cf0c613008a6b61b79bab
Harry Vasanth <mailsuthan@gmail.com>
2023-01-24 00:19:01 +0000

    chore(codebase): format and prettify

--- add_history.py
+++ add_history.py
@@ -1,16 +1,31 @@
 import json
 
-from constants import CURRENT_GAS_HISTORY_JSON_FILE, CURRENT_WEEK, START_DATE_KEY, CURRENT_GAS_HISTORY_CSV_FILE, END_DATE_KEY, COLORED_DIESEL, GASOLINE_98, GASOLINE_95, DIESEL, GAS_KEY
+from constants import (
+    CURRENT_GAS_HISTORY_JSON_FILE,
+    CURRENT_WEEK,
+    START_DATE_KEY,
+    CURRENT_GAS_HISTORY_CSV_FILE,
+    END_DATE_KEY,
+    COLORED_DIESEL,
+    GASOLINE_98,
+    GASOLINE_95,
+    DIESEL,
+    GAS_KEY,
+)
 
 
 def add_history(dict_prices):
     # JSON
-    with open(CURRENT_GAS_HISTORY_JSON_FILE, 'r') as f:
+    with open(CURRENT_GAS_HISTORY_JSON_FILE, "r") as f:
         current_data = json.load(f)
-    with open(CURRENT_GAS_HISTORY_JSON_FILE, 'w') as f:
-        current_data[dict_prices[CURRENT_WEEK][START_DATE_KEY]] = dict_prices[CURRENT_WEEK]
+    with open(CURRENT_GAS_HISTORY_JSON_FILE, "w") as f:
+        current_data[dict_prices[CURRENT_WEEK][START_DATE_KEY]] = dict_prices[
+            CURRENT_WEEK
+        ]
         content = json.dumps(current_data, indent=1, ensure_ascii=False)
         f.write(content)
     # CSV
-    with open(CURRENT_GAS_HISTORY_CSV_FILE, 'a') as f:
-        f.write(f'{dict_prices[CURRENT_WEEK][START_DATE_KEY]},{dict_prices[CURRENT_WEEK][END_DATE_KEY]},{dict_prices[CURRENT_WEEK][GAS_KEY][GASOLINE_95]},{dict_prices[CURRENT_WEEK][GAS_KEY][DIESEL]},{dict_prices[CURRENT_WEEK][GAS_KEY][COLORED_DIESEL]},{dict_prices[CURRENT_WEEK][GAS_KEY][GASOLINE_98]}\n')
+    with open(CURRENT_GAS_HISTORY_CSV_FILE, "a") as f:
+        f.write(
+            f"{dict_prices[CURRENT_WEEK][START_DATE_KEY]},{dict_prices[CURRENT_WEEK][END_DATE_KEY]},{dict_prices[CURRENT_WEEK][GAS_KEY][GASOLINE_95]},{dict_prices[CURRENT_WEEK][GAS_KEY][DIESEL]},{dict_prices[CURRENT_WEEK][GAS_KEY][COLORED_DIESEL]},{dict_prices[CURRENT_WEEK][GAS_KEY][GASOLINE_98]}\n"
+        )
--- constants.py
+++ constants.py
@@ -1,52 +1,54 @@
 # Values
-END_DATE_KEY = 'End date'
-GAS_KEY = 'Gas'
-GAS_NAME_KEY = 'Name'
-GAS_PRICE_KEY = 'Price'
-NEW_DATE_KEY = '.......'
-START_DATE_KEY = 'Start date'
-CURRENT_WEEK = 'current'
-PREVIOUS_WEEK = 'previous'
+END_DATE_KEY = "End date"
+GAS_KEY = "Gas"
+GAS_NAME_KEY = "Name"
+GAS_PRICE_KEY = "Price"
+NEW_DATE_KEY = "......."
+START_DATE_KEY = "Start date"
+CURRENT_WEEK = "current"
+PREVIOUS_WEEK = "previous"
 # Constants
 DIFFERENCE_95_98_PRICE = 0.15
 # Endpoint
-ENDPOINT = 'https://www.madeira.gov.pt/drett'
+ENDPOINT = "https://www.madeira.gov.pt/drett"
 # Span
-SPAN_ID = 'dnn_ctr9883_View_D1_dlstInformacaoOne_Conteudo_0'
+SPAN_ID = "dnn_ctr9883_View_D1_dlstInformacaoOne_Conteudo_0"
 # Files
-CURRENT_GAS_INFO_FILE = 'gas_info.json'
-CURRENT_GAS_HISTORY_JSON_FILE = 'history/gas_info_history.json'
-CURRENT_GAS_HISTORY_CSV_FILE = 'history/gas_info_history.csv'
+CURRENT_GAS_INFO_FILE = "gas_info.json"
+CURRENT_GAS_HISTORY_JSON_FILE = "history/gas_info_history.json"
+CURRENT_GAS_HISTORY_CSV_FILE = "history/gas_info_history.csv"
 # Gas
-DIESEL = 'Gasóleo Rodoviário'
-GASOLINE_95 = 'Gasolina IO95'
-GASOLINE_98 = 'Gasolina IO98'
-COLORED_DIESEL = 'Gasóleo Colorido e Marcado'
+DIESEL = "Gasóleo Rodoviário"
+GASOLINE_95 = "Gasolina IO95"
+GASOLINE_98 = "Gasolina IO98"
+COLORED_DIESEL = "Gasóleo Colorido e Marcado"
 # Gas (for Tweet)
-DIESEL_TW = 'Gasóleo         '
-GASOLINE_95_TW = 'Gasolina 95  '
-GASOLINE_98_TW = 'Gasolina 98  '
+DIESEL_TW = "Gasóleo         "
+GASOLINE_95_TW = "Gasolina 95  "
+GASOLINE_98_TW = "Gasolina 98  "
 
 # PDF_GAS_PRICE_REGEX = r'(?<=€ )([\d,]+)(?= por litro)'
-PDF_GAS_PRICE_REGEX = r'(%s|%s|%s)(?:[\.€\w ]+)(\d{1},\d{3})' % (
-        'Gasolina super sem chumbo IO 95',
-        'Gasóleo rodoviário',
-        'Gasóleo colorido e marcado'
+PDF_GAS_PRICE_REGEX = r"(%s|%s|%s)(?:[\.€\w ]+)(\d{1},\d{3})" % (
+    "Gasolina super sem chumbo IO 95",
+    "Gasóleo rodoviário",
+    "Gasóleo colorido e marcado",
 )
 
 # History plot
-HISTORY_PLOT_LABEL_GASOLINA_IO95 = 'Gasolina IO95'
-HISTORY_PLOT_LABEL_GASOLINA_IO98 = 'Gasolina IO98'
-HISTORY_PLOT_LABEL_GASOLEO_RODOVIARIO = 'Gasoleo Rodoviario'
-HISTORY_PLOT_Y_LABEL = 'Preço (€)'
-HISTORY_PLOT_X_LABEL = 'Data'
+HISTORY_PLOT_LABEL_GASOLINA_IO95 = "Gasolina IO95"
+HISTORY_PLOT_LABEL_GASOLINA_IO98 = "Gasolina IO98"
+HISTORY_PLOT_LABEL_GASOLEO_RODOVIARIO = "Gasoleo Rodoviario"
+HISTORY_PLOT_Y_LABEL = "Preço (€)"
+HISTORY_PLOT_X_LABEL = "Data"
 
 # CSV columns
-COLUMN_START_DATE = 'start_date'
-COLUMN_END_DATE = 'end_date'
-COLUMN_GASOLINA_IO95 = 'gasolina_IO95'
-COLUMN_GASOLINA_IO98 = 'gasolina_IO98'
-COLUMN_GASOLEO_RODOVIARIO = 'gasoleo_rodoviario'
+COLUMN_START_DATE = "start_date"
+COLUMN_END_DATE = "end_date"
+COLUMN_GASOLINA_IO95 = "gasolina_IO95"
+COLUMN_GASOLINA_IO98 = "gasolina_IO98"
+COLUMN_GASOLEO_RODOVIARIO = "gasoleo_rodoviario"
 
 # Tweets
-TWEET_HISTORY = 'Variação dos preços dos combustíveis na Madeira, de {start_date} a {end_date}.'
+TWEET_HISTORY = (
+    "Variação dos preços dos combustíveis na Madeira, de {start_date} a {end_date}."
+)
