7ca5841d8abf23b57a16c3fa409c8e3193a5b66c
Harry Vasanth <mailsuthan@gmail.com>
2023-01-31 01:46:21 +0000

    fix(joram): iterate links until prices extracted

--- joram.py
+++ joram.py
@@ -10,32 +10,30 @@ from constants import GASOLINE_95, DIESEL, COLORED_DIESEL, PDF_GAS_PRICE_REGEX
 current_year = str(datetime.datetime.now().year)
 
 # JORAM URL for current year's PDFs
-url = f"https://joram.madeira.gov.pt/joram/2serie/Ano%20de%20{current_year}/"
+joram_url = "https://joram.madeira.gov.pt/joram/2serie/Ano%20de%20"
+joram_current_year_url = joram_url + current_year + "/"
 
-response = requests.get(url)
+# Get the HTML response for the URL
+response = requests.get(joram_current_year_url)
 html = response.text
-soup = BeautifulSoup(html, "html.parser")
-links = soup.find_all("a")
 
-pdf_links = [link for link in links if link["href"].endswith(".pdf")]
+# Parse the HTML content using BeautifulSoup
+html_content = BeautifulSoup(html, "html.parser")
+
+# Find all the links ending with '.pdf' from the HTML page
+pdf_links = [
+    link for link in html_content.find_all("a") if link["href"].endswith(".pdf")
+]
+
+# Sort the links based on the date in the href
 sorted_pdf_links = sorted(
     pdf_links,
     key=lambda link: datetime.datetime.strptime(
         re.search(r"\d{4}-\d{2}-\d{2}", link["href"]).group(), "%Y-%m-%d"
     ),
 )
-newest_pdf_link = sorted_pdf_links[-1]
-newest_pdf_filename = newest_pdf_link["href"].split("/")[-1]
 
-# FIXME (DEBUG) Print se the filename to scrape the content
-print(newest_pdf_filename)
-# FIXME (DEBUG): Working link to Joram PDF
-debug_pdf_joram = "https://joram.madeira.gov.pt/joram/2serie/Ano%20de%202022/IISerie-138-2022-07-22Supl.pdf"
-
-# Get the latest PDF from the JORAM website
-newest_pdf_joram = f"https://joram.madeira.gov.pt/joram/2serie/Ano%20de%20{current_year}/{newest_pdf_filename}"
-
-
+# Generator function to extract line by line text from PDF
 def get_pdf_content_lines(pdf_raw_data):
     with BytesIO(pdf_raw_data) as f:
         pdf_reader = PdfReader(f)
@@ -44,23 +42,28 @@ def get_pdf_content_lines(pdf_raw_data):
                 yield line
 
 
-def read_pdf_prices(url):
-    """
-    Reads a pdf line by line and verify if contains a gas price.
-    The prices are always in the same order just need to make a match.
-    """
+# Function to extract the gas prices from the PDFs
+def read_pdf_prices(joram_current_year_url):
     discovered_prices = 0
-    response = requests.get(url)
+    response = requests.get(joram_current_year_url)
     for line in get_pdf_content_lines(response.content):
         if discovered_prices == 3:
             break
-        matches = re.search(PDF_GAS_PRICE_REGEX, line)
-        if matches:
+        match = re.search(PDF_GAS_PRICE_REGEX, line)
+        if match:
             discovered_prices += 1
-            yield matches.groups()
+            yield match.groups()
 
 
 if __name__ == "__main__":
-    print(dict(read_pdf_prices(newest_pdf_joram)))
-    # FIXME (DEBUG): Print the hardcoded link
-    print(dict(read_pdf_prices(debug_pdf_joram)))
+    # Loop through the sorted PDF links and find the gas prices
+    while len(sorted_pdf_links) > 0:
+        newest_pdf_link = sorted_pdf_links.pop()
+        newest_pdf_filename = newest_pdf_link["href"].split("/")[-1]
+        newest_pdf_joram = joram_url + current_year + "/" + newest_pdf_filename
+        gas_prices = dict(read_pdf_prices(newest_pdf_joram))
+        if gas_prices:
+            break
+    print(gas_prices)
+    print(newest_pdf_filename)
+    print(newest_pdf_joram)
