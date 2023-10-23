12c2be07ed5d42495277f6439cc323ba7b043069
Carlos Abreu <carlosrsabreu@gmail.com>
2023-10-23 23:45:12 +0100

    chore: fix gas dates

--- functions.py
+++ functions.py
@@ -6,7 +6,12 @@ from constants import COLORED_DIESEL, DIESEL, GASOLINE_95
 def retrieve_week_by_date(date):
     start_of_the_week = date - datetime.timedelta(days=date.weekday())
     end_of_the_week = start_of_the_week + datetime.timedelta(days=6)
-    return [str(start_of_the_week), str(end_of_the_week)]
+    return [start_of_the_week.strftime('%Y-%m-%d') , end_of_the_week.strftime('%Y-%m-%d')]
+
+# We give a date, it returns the following week
+def return_next_week_by_date(date):
+    end_date = date + datetime.timedelta(weeks=1)
+    return end_date
 
 # Replace gas keys names. This function can be optimized
 def replace_gas_keys_names(gas_prices):
