aae747f25c465bb9c5519a614fcab0e7abf2a9f8
Harry <mailsuthan@gmail.com>
2026-03-25 01:27:05 +0000

    fix(test_joram): optimise

--- tests/test_joram.py
+++ tests/test_joram.py
@@ -22,15 +22,19 @@ def test_get_sorted_pdf_links():
         assert links[1]["href"] == "http://example.com/2024-05-21.pdf"
 
 
-@patch("joram.get_pdf_content_lines")
+@patch("joram.PdfReader")
 @patch("requests.get")
-def test_read_pdf_prices(mock_get, mock_get_pdf_lines):
+def test_read_pdf_prices(mock_get, mock_pdf_reader):
+    mock_get.return_value.status_code = 200
     mock_get.return_value.content = b"fake pdf content"
-    mock_get_pdf_lines.return_value = [
-        "Gasolina super sem chumbo IO 95... 1,751",
-        "Gasóleo rodoviário... 1,521",
-        "Gasóleo colorido e marcado... 1,144",
-    ]
+
+    mock_page = MagicMock()
+    mock_page.extract_text.return_value = (
+        "Gasolina super sem chumbo IO 95... 1,751\n"
+        "Gasóleo rodoviário... 1,521\n"
+        "Gasóleo colorido e marcado... 1,144\n"
+    )
+    mock_pdf_reader.return_value.pages = [mock_page]
 
     prices = dict(read_pdf_prices("http://example.com/test.pdf"))
 
